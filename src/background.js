bf678d9d156b03fa715ce5850ec0c8bf0acd0001
Pavel Tvaladze <reddragonsportal@gmail.com>
2021-02-23 03:21:12 +0300

    Manifest V3 migration

--- src/background.js
+++ src/background.js
@@ -2,36 +2,20 @@ const MENU_ID_PAGE = "ren-open-in-incognito:page";
 const MENU_ID_LINK = "ren-open-in-incognito:link";
 
 function clickAction(info) {
-  const url = info.linkUrl || info.pageUrl;
+  const url = info.linkUrl ?? info.pageUrl;
 
-  chrome.windows.getAll(
-    {
-      windowTypes: ["normal"],
-    },
-    (windows) => {
-      const incognitoWin = windows.find((w) => w.incognito === true);
-      const incognitoId = incognitoWin ? incognitoWin.id : null;
+  chrome.windows.getAll({ windowTypes: ["normal"] }).then((windows) => {
+    const incognitoWin = windows.find((w) => w.incognito === true);
+    const incognitoId = incognitoWin?.id ?? null;
 
-      if (incognitoId !== null) {
-        chrome.tabs.create(
-          {
-            windowId: incognitoId,
-            url: url,
-          },
-          (tab) => {
-            chrome.tabs.update(tab.id, {
-              active: true,
-            });
-          }
-        );
-      } else {
-        chrome.windows.create({
-          url: url,
-          incognito: true,
-        });
-      }
+    if (incognitoId !== null) {
+      chrome.tabs.create({ windowId: incognitoId, url: url }).then((tab) => {
+        chrome.tabs.update(tab.id, { active: true });
+      });
+    } else {
+      chrome.windows.create({ url: url, incognito: true });
     }
-  );
+  });
 }
 
 chrome.contextMenus.removeAll(() => {
